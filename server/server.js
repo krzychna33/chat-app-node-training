704a32db37ceccfb4dddc7b3687ca5e3e78b5150
krzychna33 <krzychna33@gmail.com>
2017-12-03 23:23:55 +0100

    Add geolocation

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message.js')
+const {generateMessage, generateLocationMessage} = require('./utils/message.js')
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -26,12 +26,12 @@ io.on('connection', (socket) =>{
     console.log('New Message: ', message)
     io.emit('newMessage', generateMessage(message.from, message.text));
     callback('this is from the server');
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
   });
+
+  socket.on('createLocation', (coords) =>{
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
+  });
+
 
   socket.on('disconnect', () =>{
     console.log('User Disconnected');
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message.js');
+var {generateMessage, generateLocationMessage} = require('./message.js');
 
 describe('generateMessage', ()=>{
   it('Should generate correct message object', () =>{
@@ -11,6 +11,21 @@ describe('generateMessage', ()=>{
 
     expect(response.from).toBe(from);
     expect(response.text).toBe(text);
+    expect(response.createdAt).toBeA('number');
+  });
+});
+
+describe('generateLocationMessage', () =>{
+  it('Should generate correct Location Message', () =>{
+    var from = "Jen";
+    var latitude = 1;
+    var longitude = 1;
+    var url = 'https://www.google.com/maps?q=1,1'
+
+    var response = generateLocationMessage(from, latitude, longitude);
+
+    expect(response.from).toBe(from);
+    expect(response.url).toBe(url);
     expect(response.createdAt).toBeA('number');
   });
 });
