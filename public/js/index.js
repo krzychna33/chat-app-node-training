2d3ff63ed518aa6b8a522ed6f999e38aedc950e8
krzychna33 <krzychna33@gmail.com>
2017-12-06 23:19:12 +0100

    Add CSS

--- public/js/index.js
+++ public/js/index.js
@@ -28,15 +28,16 @@ socket.on('newLocationMessage', function(message){
   jQuery('#messages').append(li);
 });
 
+var messageTextBox = jQuery('[name=message]');
 
 jQuery('#message-form').on('submit', function(e){
   e.preventDefault();
 
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, function(){
-
+    messageTextBox.val('');
   });
 });
 
@@ -47,12 +48,16 @@ locationButton.on('click', function(){
       return alert('Geolocation not supported by your browser.')
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...')
+
     navigator.geolocation.getCurrentPosition(function(position){
+      locationButton.removeAttr('disabled').text('Send location');
       socket.emit('createLocation', {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       })
     }, function(){
+      locationButton.removeAttr('disabled').text('Send location');
       alert('Unable to fetch location.')
     });
 });
